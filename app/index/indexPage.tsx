39c80845124de7b2f6a341c682bb27768873859b
Danil-V-uni <danil.vanichkin@ucalgary.ca>
2026-03-19 20:38:22 -0600

    Add index page heading and about text

--- app/index/indexPage.tsx
+++ app/index/indexPage.tsx
@@ -7,11 +7,19 @@ export function IndexPage() {
         <main>
             <div className="navBar">
                 <>
-                <Link to='/history' className="navButton">History</Link>
-                <Link to='/account' className="navButton">Account</Link>
-                <Link to='/login' className="navButton">Login/Register</Link>
-                <Link to='/quiz' className="navButton">Take Quiz</Link>
+                    <Link to='/history' className="navButton">History</Link>
+                    <Link to='/account' className="navButton">Account</Link>
+                    <Link to='/login' className="navButton">Login/Register</Link>
+                    <Link to='/quiz' className="navButton">Take Quiz</Link>
                 </>
+            </div>
+            <div>
+                <h1 className="text-5xl font-bold text-center mt-20">
+                    IQ Test App
+                </h1>
+                <p className="text-center mt-5">
+                    An LLM based web application for procedurally generating IQ tests, keeping track of test history, and more!
+                </p>
             </div>
         </main>
     )
