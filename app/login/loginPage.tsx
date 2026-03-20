c9c192d2b16f4877627d4c18f8b9eb4d87a11e87
Lreed03 <lreed0383@gmail.com>
2026-03-20 13:04:41 -0600

    Add redirect to login page

--- app/login/loginPage.tsx
+++ app/login/loginPage.tsx
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { supabase } from "../supabase";
+import { useNavigate } from "react-router";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkUser = async () => {
+      const { data } = await supabase.auth.getUser();
+
+      if (data.user) {
+        navigate("/");
+      }
+    };
+
+    checkUser();
+  }, []);
 
   const handleLogin = async () => {
     const { data, error } = await supabase.auth.signInWithPassword({
@@ -11,14 +25,17 @@ export default function LoginPage() {
       password,
     });
 
+    console.log("LOGIN RESULT:", { data, error });
+
     if (error) {
       alert(error.message);
       return;
     }
 
-    console.log("Logged in:", data.user);
-  };
+    alert("Logged in!");
 
+    navigate("/"); 
+  };
   const handleSignup = async () => {
     const { data, error } = await supabase.auth.signUp({
       email,
