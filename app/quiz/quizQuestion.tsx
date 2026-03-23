4765872f0f2ce60e6d5049434ed7bc397ae50158
Chase MacKenzie <chasemack@shaw.ca>
2026-03-22 19:31:09 -0600

    QuizPage bug fix. Previous question no longer acts like submit quiz button when on final page

--- app/quiz/quizQuestion.tsx
+++ app/quiz/quizQuestion.tsx
@@ -35,28 +35,29 @@ export function QuizQuestion({question, onNext, onPrevious, hasNext, hasPrevious
             </div>
             <div>
                 <form onSubmit={(event) => {
-                    window.confirm("Are you finished with your quiz?");
                     event.preventDefault();
-                    onSubmit(selectedValue);
+                    if(window.confirm("Are you finished with your quiz?")) {
+                        onSubmit(selectedValue);
+                    }
                 }}>
                     <legend>Select one of the following answers:</legend>
                     <label>
-                    <input type="radio" id="a" name="a" value={question.options[0]}
+                    <input type="radio" id="a" name="answer" value={question.options[0]}
                     checked={selectedValue === question.options[0]} onChange={handleChange}/>{question.options[0]}
                     </label><br/>
                     <label>
-                    <input type="radio" id="b" name="b" value={question.options[1]}
+                    <input type="radio" id="b" name="answer" value={question.options[1]}
                      checked={selectedValue === question.options[1]} onChange={handleChange}/>{question.options[1]}
                     </label><br/>
                     <label>
-                    <input type="radio" id="c" name="c" value={question.options[2]}
+                    <input type="radio" id="c" name="answer" value={question.options[2]}
                      checked={selectedValue === question.options[2]} onChange={handleChange}/>{question.options[2]}
                     </label><br/>
                     <label>
-                    <input type="radio" id="d" name="d" value={question.options[3]}
+                    <input type="radio" id="d" name="answer" value={question.options[3]}
                      checked={selectedValue === question.options[3]} onChange={handleChange}/>{question.options[3]}
                     </label><br/>
-                    {hasPrevious && <button onClick={onPrevious}>Previous Question</button>}
+                    {hasPrevious && <button type="button" onClick={onPrevious}>Previous Question</button>}
                     {hasNext && <button type="button" onClick={handleNext}>Next Question</button>}
                     {!hasNext && <button type="submit">Submit Quiz</button>}
                 </form>
